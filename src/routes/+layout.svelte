4f15e497364ede9cb1371bdd42623fce5725f9b1
Onni Ahopelto <onni.ahopelto@gmail.com>
2024-07-29 14:19:37 +0300

    Initial

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import '../app.css';
+	const { children } = $props();
+</script>
+
+<div class="h-screen w-screen bg-slate-100">
+	<nav>
+		<a>Logo</a>
+		<a>Profile</a>
+		<a>Signin</a>
+	</nav>
+	<div>
+		{@render children()}
+	</div>
+</div>
