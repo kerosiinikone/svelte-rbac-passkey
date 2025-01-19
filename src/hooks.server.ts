380f7acbbdac64658e193bd104abfd127eb8efaf
kerosiinikone <onni.ahopelto@gmail.com>
2025-01-19 14:22:46 +0200

    PNPM migration and JWT hook logic refactoring

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,18 +1,25 @@
-import { JWT_SECRET } from '$env/static/private';
 import { getUserById } from '$lib/server/operations/users.operations';
-import { logout, setCookies, signTokenPayload } from '$lib/server/utils/auth';
+import { logout, setCookies, signTokenPayload, verifyJWT } from '$lib/server/utils/auth';
 import type { CookieParameters, TokenResponse } from '$lib/types';
 import { type Cookies } from '@sveltejs/kit';
-import jwt from 'jsonwebtoken';
 
 type AuthUser = {
 	id: string;
 };
 
-// Routes that require authentication
+type RefreshTokenPayload = {
+	id: string;
+	version: number;
+	exp: number;
+}
+
+type AccessTokenPayload = {
+	id: string;
+	exp: number;
+}
+
 const protectedRoutes = ['/', '/profile'];
 
-// Main hook that runs on server requests
 export async function handle({ event, resolve }) {
 	const accessToken = event.cookies.get('accessToken');
 
@@ -25,114 +32,71 @@ export async function handle({ event, resolve }) {
 				event.locals.user = user?.id;
 			}
 		} catch (error) {
+			console.error('Error verifying user:', error);
 			logout(event.cookies, event.locals);
 		}
 	}
-
-	const response = await resolve(event);
-	return response;
+	return await resolve(event);
 }
 
-// Match the protected routes to the incoming req path
-const protectedRouteMatcher = (path: string, routes: string[]): boolean =>
-	routes.some((r) => path.startsWith(r));
-
-// Get a new set of tokens
 async function refreshTokens(refreshToken: string): Promise<TokenResponse | null> {
-	let verifiedRefreshPayload;
-
 	try {
-		verifiedRefreshPayload = jwt.verify(refreshToken, JWT_SECRET) as {
-			id: string;
-			version: number;
-			exp: number;
-		};
-	} catch (e) {
-		return null;
-	}
-
-	try {
+		const verifiedRefreshPayload = verifyJWT<RefreshTokenPayload>(refreshToken);
 		const user = await getUserById(verifiedRefreshPayload.id);
 
-		if (!user) {
-			return null;
-		}
-
-		if (verifiedRefreshPayload.version !== user.token_version) {
-			return null;
-		}
-
-		// Sign new tokens
-		const { accessToken, refreshToken: newRefreshToken } = signTokenPayload(
-			user.id,
-			user.token_version + 1
-		);
-
-		return {
-			accessToken,
-			refreshToken: newRefreshToken
-		};
+		if (!user || verifiedRefreshPayload.version !== user.token_version) {
+            return null;
+        }
+        const { accessToken, refreshToken: newRefreshToken } = signTokenPayload(
+            user.id,
+            user.token_version + 1
+        );
+        return {
+            accessToken,
+            refreshToken: newRefreshToken
+        };
 	} catch (error) {
-		// DB Error
 		return null;
 	}
 }
 
-// Refresh logic to revalidate the user auth
-async function attemptTokenRefresh(cookies: Cookies): Promise<AuthUser | null> {
+async function handleRefreshToken(cookies: Cookies): Promise<AuthUser | null> {
 	const initialRefreshToken = cookies.get('refreshToken');
-
 	if (!initialRefreshToken) return null;
 
-	const tokenSet = await refreshTokens(initialRefreshToken);
-
-	if (!tokenSet) return null;
-
-	const { accessToken, refreshToken } = tokenSet;
-
-	try {
-		const loggedInUser = jwt.verify(accessToken, JWT_SECRET) as {
-			id: string;
-			exp: number;
-		};
-
-		if (!loggedInUser.id) {
-			return null;
-		}
-
+	const tokens = await refreshTokens(initialRefreshToken);
+    if (tokens) {
 		const cookieList: CookieParameters[] = [
-			{ name: 'accessToken', val: accessToken, opts: { maxAge: 7 * 24 * 60 * 60 * 1000 } },
-			{ name: 'refreshToken', val: refreshToken, opts: { maxAge: 15 * 60 * 1000 } }
+			{ name: 'accessToken', val: tokens.accessToken, opts: { maxAge: 7 * 24 * 60 * 60 * 1000 } },
+			{ name: 'refreshToken', val: tokens.refreshToken, opts: { maxAge: 15 * 60 * 1000 } }
 		];
-
-		setCookies(cookies, cookieList);
-
-		return { id: loggedInUser.id };
-	} catch (e) {
-		return null;
-	}
+        setCookies(cookies, cookieList);
+		
+		const payload = verifyJWT<AccessTokenPayload>(tokens.accessToken);
+        const user = await getUserById(payload.id);
+        return user ? { id: user.id } : null;
+    }
+    return null;
 }
 
-// Verify the user auth with the access token
-async function verifyAndFetchUser(accessToken: string, cookies: Cookies): Promise<AuthUser | null> {
-	let verifiedAccessPayload;
-
-	try {
-		verifiedAccessPayload = jwt.verify(accessToken, JWT_SECRET) as {
-			id: string;
-			exp: number;
-		};
-	} catch (e) {
-		return await attemptTokenRefresh(cookies);
-	}
-	try {
-		const fetchedUser = await getUserById(verifiedAccessPayload.id);
-
-		if (!fetchedUser) return null;
-
-		return { id: fetchedUser.id };
-	} catch (error) {
-		// DB Error
-		return null;
-	}
+async function verifyAndFetchUser(token: string, cookies: Cookies): Promise<AuthUser | null> {
+    try {
+        const payload = verifyJWT<AccessTokenPayload>(token);
+        if (payload.exp < Date.now() / 1000) {
+			console.log('Token expired, refreshing...');
+			return await handleRefreshToken(cookies);
+        }
+        const user = await getUserById(payload.id);
+        return user ? { id: user.id } : null;
+    } catch (error) {
+        console.error('Error verifying token:', error);
+        const refreshToken = cookies.get('refreshToken');
+        if (refreshToken) {
+            return await handleRefreshToken(cookies);
+        }
+        return null;
+    }
 }
+
+const protectedRouteMatcher = (path: string, routes: string[]): boolean =>
+	routes.some((r) => path.startsWith(r));
--- src/lib/server/utils/auth.ts
+++ src/lib/server/utils/auth.ts
@@ -3,12 +3,26 @@ import type { CookieParameters, TokenResponse } from '$lib/types';
 import type { Cookies } from '@sveltejs/kit';
 import jwt from 'jsonwebtoken';
 
+type TokenPayload = {
+	exp: number;
+}
+
 const DEFAULT_COOKIE_OPTS = {
 	secure: true,
 	httpOnly: true,
 	path: '/',
 	sameSite: 'lax' as boolean | 'lax' | 'strict' | 'none' | undefined
 };
+
+
+export const verifyJWT = <T extends TokenPayload>(token: string): T => {
+	try {
+		return jwt.verify(token, JWT_SECRET) as T;
+	} catch (error) {
+		throw new Error('Your token has expired.');
+	}
+};
+
 
 export function setCookies(writer: Cookies, cookies: CookieParameters[]): void {
 	cookies.map((c) => {
