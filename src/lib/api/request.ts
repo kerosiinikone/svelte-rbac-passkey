c566ffedba6bda17fdf6d76bfaca5507bedc2b6a
kerosiinikone <onni.ahopelto@gmail.com>
2025-01-20 11:26:56 +0200

    A more robust API caller with AbortController

--- src/lib/api/request.ts
+++ src/lib/api/request.ts
@@ -1,24 +1,45 @@
-// Wrapper for all requests
-
-// TODO: Options, headers, accessToken, etc abstracted here -> move to utils later and use on all requests
-
 type RequestMethod = 'GET' | 'POST';
 
-export function createCaller(f: (url: string, init?: RequestInit) => Promise<Response>) {
-	return async function <T>(
-		url: string,
-		method: RequestMethod = 'GET',
-		opts: RequestInit & {
-			includeHeaders?: boolean;
-		} = {}
-	) {
-		const res = await f(url, { method, ...opts });
-		if (!res.ok) {
-			throw new Error('apiFetch error');
-		}
-		return {
-			...(await res.json()),
-			headers: opts.includeHeaders ? res.headers : undefined
-		} as Promise<T>;
-	};
+interface FetchOptions extends RequestInit {
+    includeHeaders?: boolean;
+    timeout?: number;
+}
+
+export function createCaller(f: (url: string, init?: RequestInit) => Promise<Response>, defaultHeaders: HeadersInit = {}) {
+    return async function <T>(
+        url: string,
+        method: RequestMethod = 'GET',
+        opts: FetchOptions = {}
+    ): Promise<T> {
+        const { includeHeaders, timeout, ...fetchOpts } = opts;
+
+        const controller = new AbortController();
+        const id = setTimeout(() => controller.abort(), timeout || 5000);
+
+        const headers = {
+            ...defaultHeaders,
+            ...fetchOpts.headers
+        };
+
+        try {
+            const res = await f(url, { method, ...fetchOpts, headers, signal: controller.signal });
+            clearTimeout(id);
+
+            if (!res.ok) {
+                const errorText = await res.text();
+                throw new Error(`apiFetch error: ${res.status} ${res.statusText} - ${errorText}`);
+            }
+
+            const data = await res.json();
+            return {
+                ...data,
+                headers: includeHeaders ? res.headers : undefined
+            } as T;
+        } catch (error) {
+            if (controller.signal.aborted && error instanceof DOMException) {
+                throw new Error('apiFetch error: Request timed out');
+            }
+            throw error;
+        }
+    };
 }
